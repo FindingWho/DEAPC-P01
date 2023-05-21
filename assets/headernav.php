db5ffba9f10aab5147bfded20f018ca5de14a5de
Rodrigo Pires <1210697@isep.ipp.pt>
2023-05-21 23:34:11 +0100

    files for the navbar and footer were created so they can be implemented in every page of the website

---
+++ assets/headernav.php
@@ -0,0 +1,13 @@
+<nav class="menu-nav">
+    <div class="container">
+        <div class="wrapper">
+            <div class="logo">
+                <img src="images/logo.png" alt="">
+            </div> <!-- logo -->
+
+            <div class="site_name">
+                <h1>What's In My Kitchen</h1>
+            </div> <!-- site name -->
+        </div> <!-- wrapper -->
+    </div> <!-- container -->
+</nav> <!-- menu nav -->            
