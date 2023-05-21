e5ab6edc42b14d88eeedd6d2012c113424648890
Rodrigo Pires <1210697@isep.ipp.pt>
2023-05-21 21:06:22 +0100

    Files needed to start building the pages from the wireframes

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>P01</title>
+</head>
+<body>
+    <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/assets"; include ($IPATH."headernav.php") ?>
+    <?php include ($IPATH."footer.php") ?>
+</body>
+</html>
