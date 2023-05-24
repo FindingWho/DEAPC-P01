7aba6616c94e97973d41a87656adca0afb0c223b
Amir <1221579@isep.ipp.pt>
2023-05-24 16:04:45 +0330

    contacts page

---
+++ contacts.php
@@ -0,0 +1,80 @@
+<?php require_once('assets/init.php'); ?>
+
+<body>
+  <?php require_once('assets/headernav.php'); ?>
+
+  <body>
+
+    <div class="container">
+      <header>
+        <h1>All about our team</h1>
+      </header>
+      <article class="isep-logo">
+        <h2>Who are we?</h2>
+        <img src="images/Isep-logo.png" alt="Isep-logo" width="500" height="300" />
+        <p>
+
+          We are a dedicated group of <i>three</i> individuals who are currently pursuing our studies at <b>ISEP</b>
+          University.
+          With a shared passion for technology and innovation, we have come together to embark on an exciting journey of
+          developing a groundbreaking website.
+          Our mission is to create an exceptional online platform that caters to the evolving needs of users, offering
+          them a seamless and immersive experience.
+          Drawing upon our diverse skill sets and educational backgrounds, we collaborate closely to ensure that our
+          website incorporates cutting-edge design, user-friendly functionality,
+          and robust performance. Our commitment to excellence drives us to constantly refine and enhance our work,
+          pushing boundaries to deliver a product that exceeds expectations.
+        </p>
+
+      </article>
+      <h3>Our group</h3>
+
+      <article class="our-pics">
+        <ul>
+          <li class="article">
+            <img src="images/Rodrigo.PNG" alt="Rodrigo" width="200" height="200" />
+            <p>
+              <b>Rodrigo</b> is a creative and visionary thinker, specializing in graphic design and user experience.
+              With an eye for aesthetics and a deep understanding of user behavior, he strives to create visually
+              stunning and intuitive interfaces that captivate and engage users.
+              Rodrigo brings a unique blend of artistic talent and technical proficiency to the team, ensuring that the
+              website delivers an exceptional visual experience while maintaining optimal usability.
+              His ability to translate complex ideas into compelling visuals sets our website apart from the rest.
+            </p>
+
+          </li>
+          <li class="article">
+            <img src="images/Amir.jpg" alt="Amir" width="200" height="200" />
+            <p>
+              <b>Amir</b> is a dedicated and innovative individual with a strong background in computer science. His
+              expertise lies in web development and programming,
+              and he is always up-to-date with the latest trends and technologies in the field.
+              With his meticulous attention to detail and problem-solving skills, Amir ensures that the website is not
+              only visually appealing but also functions flawlessly.
+              His passion for creating efficient and user-friendly interfaces shines through in his work, making him an
+              invaluable asset to our team.
+            </p>
+          </li>
+          <li class="article">
+            <img src="images/" alt="Joana" />
+            <p>
+              <b>Joana</b> is a highly skilled and detail-oriented individual, specializing in content creation and
+              project management.
+              With her strong organizational skills and effective communication, she ensures that the website's content
+              is not only informative but also engaging.
+              Joana conducts thorough research and collaborates closely with the team to develop compelling copy and
+              multimedia elements that resonate with the target audience.
+              Her commitment to delivering high-quality content that aligns with the website's goals and objectives is
+              instrumental in creating a cohesive and impactful user experience.
+            </p>
+          </li>
+        </ul>
+      </article>
+  </body>
+
+  </html>
+
+  </div>
+
+  <?php require_once('assets/footer.php') ?>
+</body>
