e5ae7a3b29ef3f6225e8ae70589619c241f52bc7
Joana Sousa <jgsousa2003@gmail.com>
2023-06-07 19:02:57 +0100

    Login and Register completed
     HTML, CSS, JAVASCRIPT

--- register.php
+++ register.php
@@ -7,31 +7,32 @@
 	<?php require_once('assets/init.php') ?>
 	<link rel="stylesheet" href="css/login.css">
 	<?php require_once('connect.php') ?>
-	<script type="text/javascript" src="scripts/login.js"></script>
+    <script type="text/javascript" src="scripts/register.js"></script>
 </head>
 <body>
-    <div class="login" align="center">
+    <div class="register" align="center">
     	<i class="fa-solid fa-circle-user"></i>
     	<form class="login_form">
-			</br>
-    		<b><label class="title">Sign In</label></b></br>
+            </br>
+    		<b><label class="title">Create Account</label></b><br>
 
-			</br><img class="icon" src="images/user.svg">
-			<input type="email" id="Email" class="textbox" placeholder="Email" autofocus require></input></br>
+            <input type="text" id="Name" class="textbox" placeholder="Name" autofocus require></input></br>
+			<p class = "errorMessageName" ></p></br>
+
+            <input type="email" id="Email" class="textbox" placeholder="Email" autofocus require></input></br>
 			<p class = "errorMessageEmail" ></p></br>
 
-			<img class="icon" src="images/lock.svg">
 			<input type="password" id="Pass" class="textbox" placeholder="Password" require></br>
 			<p class = "errorMessagePass" ></p></br>
 
-    		<input type="button" id="button" value="Login" onclick="validate_form()">
+    		<input type="button" id="button" value="Create" onclick="validate_form()">
     	</form>
 	</br>
 	<div class="line">
 		<span>OR</span>
 	</div>
 	</br>
-	<a class="link" href="register.php">New? Create Account</a>
+	<a class="link" href="login.php">Have an account? Sign In</a>
 	</br></br>
 	<a class="link" href="index.php">Continue as Guest</a>
 </body>
